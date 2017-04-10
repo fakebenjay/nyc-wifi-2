f1ace51bd91943628e61abfc00d335a5df697dd9
fakebenjay <fakebenjay@gmail.com>
2017-04-10 11:07:38 -0400

    minor test change

--- js/wrappers/hotspotApi.js
+++ js/wrappers/hotspotApi.js
@@ -1,8 +1,8 @@
 class HotspotApi {
   static getJSON(resource) {
     return $.getJSON(`https://data.cityofnewyork.us/api/${resource}`)
-    .then((resp) => {
-      return resp.data
+    .then((response) => {
+      return response.data
     })
   }
 }
