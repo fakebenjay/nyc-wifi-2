e9a5ad803100095af410c1743a208eac158a37b2
fakebenjay <fakebenjay@gmail.com>
2017-04-10 15:48:22 -0400

    Configged var

--- js/wrappers/googleAPI.js
+++ js/wrappers/googleAPI.js
@@ -1,7 +1,7 @@
 class GoogleApi {
   static apiCall() {
     return $.ajax({
-      url: `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}`,
+      url: `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}`,
       type: "GET",
       dataType: 'jsonp',
       cache: false,
