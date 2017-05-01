f564af347d6a98fea26fe3bfea389f91c6ef3b61
fakebenjay <fakebenjay@gmail.com>
2017-05-01 11:09:45 -0400

    fixed key for heroku access

--- js/wrappers/googleAPI.js
+++ js/wrappers/googleAPI.js
@@ -1,7 +1,7 @@
 class GoogleApi {
   static apiCall() {
     return $.ajax({
-      url: `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}`,
+      url: `https://maps.googleapis.com/maps/api/js?key=AIzaSyDMTb0GRf25LLNd8nYusd2sALCG0DYtLt4`,
       type: "GET",
       dataType: 'jsonp',
       cache: false,
